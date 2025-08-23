a61f2e7c51bd51500c8a0aef0af80641496e90eb
GhoshAditi <hena.ghosh786@gmail.com>
2025-08-23 17:01:12 +0530

    lng

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 
-import { Button } from "@/components/ui/button";
-import GuardianMailLogo from "@/components/icons/logo";
-import Link from "next/link";
-import Image from "next/image";
 import { Navbar } from "@/components/landing/Navbar";
 
 export default function HeroPage() {
   return (
     <div>
       <Navbar />
+      
       
       </div>
   );
